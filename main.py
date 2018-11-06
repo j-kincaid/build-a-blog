09e65adb4c7ec20d6e3a5d0726dd9d5ab19b1946
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-06 14:10:50 -0600

    added blog.html

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 # TODO: When a user submits a new post, redirect them to the main blog page.
-# TODO: Make a Blog class with the necessary properties (i.e., an id, title, and body), then initialize your database:
-
-# (flask-env) $ python
-# from main import db, Blog
-# db.create_all()
-# db.session.commit()
 
 # TODO: /blog route displays all the blog posts.
 
@@ -32,21 +26,38 @@ db = SQLAlchemy(app)
 
 
 
-class Blog(db.Model):
+# TODO: Make a Blog class with the necessary properties (i.e., an id, title, and body), then initialize your database with these:
 
+# (flask-env) $ python
+# from main import db, Blog
+# db.create_all()
+# db.session.commit()
+
+class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # The name is the column within word name
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
 
 # Create a constructor: 
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
 
-@app.route('/', methods=['POST', 'GET'])
+#     words = Blog.query.filter_by(completed=False).all()
+#     completed_words = Blog.query.filter_by(completed=True).all()
+#     return render_template('entries.html',title="Build a Blog!", 
+#         words=words, completed_words=completed_words)
+
+@app.route('/blog')
 def index():
+    return render_template('blog.html')
+
+@app.route('/blog', methods=['POST'])
+def add_entry():
 
     if request.method == 'POST':
         blog_name = request.form['word']
@@ -54,28 +65,12 @@ def index():
         db.session.add(new_blog)
         db.session.commit()
 
-# Change template name
+    return render_template('entries.html')
 
-    words = Blog.query.filter_by(completed=False).all()
-    completed_words = Blog.query.filter_by(completed=True).all()
-    return render_template('entries.html',title="Build a Blog!", 
-        words=words, completed_words=completed_words)
-
-
-@app.route('/blog', methods=['POST'])
-def add_entry():
-
-    word_id = int(request.form['word-id'])
-    word = Blog.query.get(word_id)
-    word.completed = True
-    db.session.add(word)
-    db.session.commit()
-
-    return redirect('/')
 
 
 @app.route('/newpost', methods=['POST'])
-def delete_word():
+def process_add_entry():
 
     word_id = int(request.form['word-id'])
     word = word.query.get(word_id)
