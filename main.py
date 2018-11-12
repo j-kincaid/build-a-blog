33ae7f9bee9579368c4e1ff550dfdf0ed34fec05
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-12 15:12:37 -0600

    added comments, deleted error

--- main.py
+++ main.py
@@ -7,25 +7,34 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:introducin
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-# TODO: Make a Blog class with the necessary properties (i.e., an id, title, and body)
-# 
-# TODO: Initialize your database with these:
-
-# (flask-env) $ python
-# from main import db, Blog
-# db.create_all()
-# db.session.commit()
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # The name is the column within blog name
-    title = db.Column(db.String(500))
-    body = db.Column(db.Text)
+class Blog(db.Model): # Create an instance of the Blog class
+    id = db.Column(db.Integer, primary_key=True) # Cretes a new property of our class that will map to an integer column in the blog table. The column name will be generated from the property name to be id as well. The column will be a primary key column on the table.
+    #The name is the column within blog name
+    title = db.Column(db.String(500)) # Creates a property that will map to a column of type VARCHAR(120) in the blog table.
+    body = db.Column(db.String(1000))
 
 # Create a constructor: 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+        blog = request.form['blog']
+        new_blog = Blog(blog) # To create an instance of our persistent Blog class, we use the same syntax as always.
+        db.session.add(new_blog) # Store the object in the database
+        db.session.commit() # commit it to the db
+
+
+    blogs = Blog.query.filter_by(body=False).all() # Here, Blog.query.all() has the net effect of running SELECT * FROM blog and then taking the results and turning them into a list of Blog objects.
+
+    # only give me the blogs for which the body column has the value False
+    body_blogs = Blog.query.filter_by(body=True).all()
+    return render_template('newpost.html',title="Add a New Entry!", blogs=blogs, body_blogs=body_blogs)
+
 
 # @app.route('/blog') # The blog route displays all posts.
 # def index():
@@ -35,38 +44,26 @@ class Blog(db.Model):
 def add_entry():
 
     if request.method == 'POST':
-        post_name = request.form['post']
-        new_post = Blog(post_name)
-        db.session.add(new_post)
+        blog_name = request.form['post']
+        new_blog = Blog(blog_name)
+        db.session.add(new_blog)
         db.session.commit()
-
-    return render_template('newpost.html',title="Add a Post!", posts=posts, new_posts=new_posts)
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-
-    if request.method == 'POST':
-        post_name = request.form['post']
-        new_post = Blog(post_name) # Create a new post object
-        db.session.add(new_post)
-        db.session.commit() # commit it to the db
 
     return render_template('blog.html')
 
-@app.route('/newpost', methods=['POST']) # Submit your posts through '/newpost' 
+
+@app.route('/newpost', methods=['POST']) # Submit your blogs through '/newpost' 
 # After you submit, the main page is displayed.
 def process_add_entry():
 
-    post_id = int(request.form['post-id'])
-    post = post.query.get(post_id)
-    post.completed = True
-    db.session.add(post)
+    blog_id = int(request.form['blog-id'])
+    blog = Blog.query.get(blog_id) # Calling query.get() will query for the specific object/row by its primary key.
+    blog.body = True
+    db.session.add(blog)
     db.session.commit()
 
-    return redirect('/')
+    return redirect('/blog')
 
-    
 if __name__ == '__main__':
 
     app.run()
