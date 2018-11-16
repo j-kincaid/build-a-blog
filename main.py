278125598b74ba62552e66a4cddb5b8fc13e6d3f
Jessica Kincaid <4j.kincaid@gmail.com>
2018-11-15 21:12:30 -0600

    create post.html

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Blog(db.Model): # Create an instance of the Blog class
     id = db.Column(db.Integer, primary_key=True) # Cretes a new property of our class that will map to an integer column in the blog table. The column name will be generated from the property name to be id as well. The column will be a primary key column on the table.
     #The name is the column within blog name
     title = db.Column(db.String(500)) # Creates a property that will map to a column of type VARCHAR(120) in the blog table.
-    body = db.Column(db.String(1000))
+    body = db.Column(db.String(2000))
 
 # Create a constructor: 
     def __init__(self, title, body):
@@ -19,50 +19,68 @@ class Blog(db.Model): # Create an instance of the Blog class
         self.body = body
 
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    id=request.args.get('id')
+    if id:
+        blog= Blog.query.filter_by(id=id).first()
+        return render_template('post.html', blog=blog)
+    blogz = Blog.query.all()
+    return render_template('blog.html', blogz=blogz)
 
-    if request.method == 'POST':
-        blog = request.form['blog']
-        new_blog = Blog(blog) # To create an instance of our persistent Blog class, we use the same syntax as always.
-        db.session.add(new_blog) # Store the object in the database
-        db.session.commit() # commit it to the db
-
-
-    blogs = Blog.query.filter_by(body=False).all() # Here, Blog.query.all() has the net effect of running SELECT * FROM blog and then taking the results and turning them into a list of Blog objects.
-
-    # only give me the blogs for which the body column has the value False
-    body_blogs = Blog.query.filter_by(body=True).all()
-    return render_template('newpost.html',title="Add a New Entry!", blogs=blogs, body_blogs=body_blogs)
 
 
 # @app.route('/blog') # The blog route displays all posts.
 # def index():
 #     return render_template('blog.html')
 
-@app.route('/blog', methods=['POST'])
-def add_entry():
 
-    if request.method == 'POST':
-        blog_name = request.form['post']
-        new_blog = Blog(blog_name)
-        db.session.add(new_blog)
-        db.session.commit()
-
-    return render_template('blog.html')
-
-
-@app.route('/newpost', methods=['POST']) # Submit your blogs through '/newpost' 
+@app.route('/newpost', methods=['POST','GET']) # Submit your blogs through '/newpost' 
 # After you submit, the main page is displayed.
 def process_add_entry():
+    title_error = ''
+    body_error = ''
+    title = ''
+    if request.method == 'POST': # Create a new post
+        
+        title = request.form['title']
+        body = request.form['body']
+        
+        new_blog = Blog(title, body)
+        db.session.add(new_blog)
+        db.session.commit()
+        
+        if not title:
+            title_error = 'You must enter a title.'
+        if not body:
+            body_error = 'You must enter a blog post.'
+        if not title_error and not body_error:
+            return redirect('/')
+    return render_template('newpost.html', title=title, title_error=title_error, body_error=body_error)
+    
 
-    blog_id = int(request.form['blog-id'])
-    blog = Blog.query.get(blog_id) # Calling query.get() will query for the specific object/row by its primary key.
-    blog.body = True
-    db.session.add(blog)
-    db.session.commit()
+    # def register():
+    # if request.method == 'POST': # Create a new user, looking at register.html
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     verify = request.form['verify']
 
-    return redirect('/blog')
+    #     # TODO - validate user's data
+
+    #     existing_user = User.query.filter_by(email=email).first()
+    #     if not existing_user:
+    #         new_user = User(email, password)
+    #         db.session.add(new_user)
+    #         db.session.commit()
+    #         # TODO - remember the user
+    #         session['email'] = email
+    #         return redirect('/')
+    #     else:
+    #         # TODO - better response message
+    #         return "<h1>Duplicate User</h1>"
+
+    # return render_template('register.html')
 
 if __name__ == '__main__':
 
